e78280f1660fecf1928f9035161024070cb7b610
petru-polacek <petru.polacek@gmail.com>
2022-06-28 16:19:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-sebrvm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-sebrvm)
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  mounted() {
+    console.log('Hello Jérôme');
+    console.log('Please create an object that have a property an array');
+    console.log('Please iter over all properties and over all values');
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
